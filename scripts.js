3bb1186fa948f6c2032b7eef0af3090748c16ecf
orlyohreally <orli-knop@ya.ru>
2017-06-29 09:08:24 +0300

    update 1

--- scripts.js
+++ scripts.js
@@ -6,32 +6,21 @@ $(window).scroll(function(){
 	$('.logo').css({
 		'transform': 'translate(0px, ' + wScroll / 1.2 + '%)'
 	});
-	if($(window).width() > 478) {
-		console.log("normal screen");
-		if(wScroll > $('.banner').offset().top - 100) {
-			$('.info-1').css({
-				'transform': 'translate(100%)'
-			})
-		}
-		/*else if(wScroll == $('.banner').offset().top) {
-			$('.info-1').css({
-				'transform': 'translate(-100%)'
-			})
-		}*/
-
-		if(wScroll > $('.info-1').offset().top - 250) {
-			$('.info-2').css({
-				'transform': 'translate(-100%)'
-			})
-		}
-		/*else if(wScroll < $('.info-1').offset().top + 100) {
-			$('.info-2').css({
-				'transform': 'translate(100%)'
-			})
-		}*/
-	}
-	else {
-		console.log("small screen");
+	if(wScroll > $('.banner').offset().top - 200) {
+		$('.info-1').css({
+			'transform': 'translate(100%)'
+		})
 		
+	}
+	if(wScroll > $('.banner').innerHeight() + $('.info-1').innerHeight() / 2) {
+		$('.info-2').css({
+			'transform': 'translate(-100%)'
+		})
+	}
+	
+	if(wScroll > $('.parallax-div').offset().top) {
+		$('.stuff').css({
+			'transform': 'translate(0%, 0%)'
+		})
 	}
 })
