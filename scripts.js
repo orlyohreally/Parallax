ca313df2c55fc589c11dab717c04e9de75ddb339
orlyohreally <orli-knop@ya.ru>
2017-07-02 12:00:45 +0300

    added sliders

--- scripts.js
+++ scripts.js
@@ -18,9 +18,72 @@ $(window).scroll(function(){
 		})
 	}
 	
-	if(wScroll > $('.parallax-div').offset().top) {
+	if(wScroll > $('.info-1').innerHeight() + $('.info-1').innerHeight() / 2) {
 		$('.stuff').css({
 			'transform': 'translate(0%, 0%)'
 		})
+	}
+})
+var slides = {};
+slides.active = 1;
+slides.total = 3;
+stuff = {};
+stuff.active = 1;
+stuff.total = 3;
+$(document).ready(function(){
+	$('.slider-section .left-arrow').click(function(){
+		showSlide(-1, '.slider-section', '.slide-', slides);
+		console.log("slides", slides);
+	})
+	$(".slider-section .right-arrow" ).click(function() {
+		showSlide(+1, '.slider-section', '.slide-', slides);
+		console.log("slides", slides);
+	});
+	$('.stuff-list .left-arrow').click(function(){
+		showSlide(-1, '.stuff-list', '.stuff-', stuff);
+		console.log("stuff", stuff);
+	})
+	$(".stuff-list .right-arrow" ).click(function() {
+		showSlide(+1, '.stuff-list', '.stuff-', stuff);
+		console.log("stuff", stuff);
+	});
+	function showSlide(d, primaryClass, itemClass, list) {
+		console.log(itemClass + list.active);
+		$(itemClass + list.active).removeClass("active");
+		if(d < 0) {
+			//left
+			if(list.active > 1)
+				list.active--;
+		}
+		else {
+			//right
+			if(list.active < list.total)
+				list.active++;
+		}
+		if($(window).innerWidth < 767) {
+			if(list.active > 1) {
+				$(primaryClass + " .left-arrow").css({
+					'display': 'block'
+				})
+			}
+			if(list.active == 1) {
+				$(primaryClass + " .left-arrow").css({
+					'display': 'none'
+				})
+			}
+			
+			if(list.active < list.total) {
+				$(primaryClass + " .right-arrow").css({
+					'display': 'block'
+				})
+			}
+			if(list.active == list.total) {
+				$(primaryClass + " .right-arrow").css({
+					'display': 'none'
+				})
+			}
+		}
+		$(itemClass + list.active).addClass('active');
+		console.log("current", stuff.active, list.active);
 	}
 })
