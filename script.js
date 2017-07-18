10b31219bd909148ed400f2bc2b2b392b2c72211
orlyohreally <orli-knop@ya.ru>
2017-07-19 00:01:12 +0300

    updated portfolio page

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(window).ready(function(){
-	
+	setIframe();
 })
+$(window).resize(function(){
+	setIframe();
+})
+function setIframe() {
+	var iframes = document.getElementsByTagName("iframe");
+	$("iframe").each(function(i) {
+		iframes[i].width = $(window).innerWidth() * 0.6 * 0.8;
+	})
+}
