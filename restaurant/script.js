4bb12dc64250ed67d2db8f5bd23bfbf614fb62eb
orlyohreally <orli-knop@ya.ru>
2017-08-08 00:45:53 +0300

    added a map and fixed footer

--- restaurant/script.js
+++ restaurant/script.js
@@ -1,13 +1,63 @@
+var Locations = [];
+function GetLocations() {
+	var addresses = $('#find-us .locations li');
+	console.log(addresses.length);
+	var i = 0;
+	GetLocation(i, addresses);
+}
+function GetLocation(i, addresses) {
+	var geocoder = new google.maps.Geocoder();
+	if(i < addresses.length) {
+		var address = addresses[i].textContent + ", Санкт-Петербург";
+		console.log(address);
+		geocoder.geocode({ 'address': address }, function (results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				var latitude = results[0].geometry.location.lat();
+				var longitude = results[0].geometry.location.lng();
+				console.log("Latitude: " + latitude + "\nLongitude: " + longitude);
+				Locations[i] = {'lat': latitude, 'lng': longitude};
+				GetLocation(i + 1, addresses);
+			} else {
+				console.log("Request failed.");
+				Locations[i] = 0;
+				GetLocation(i + 1, addresses);
+			}
+		});
+	}
+	else
+		showMap();
+};
+var marker, map;
+function showMap() {
+	
+	map = new google.maps.Map(document.getElementById('map'), {
+	  zoom: 10,
+	  center: {lat: Locations[0].lat, lng: Locations[0].lng}
+	});
+	
+	for(var i = 0; i < Locations.length; i++) {
+		var address = {lat: Locations[i].lat, lng: Locations[i].lng};
+		marker = new google.maps.Marker({
+		  position: address,
+		  map: map
+		});
+		marker.setMap(map);
+	}
+}
+function showLocation(i) {
+	var address = {lat: Locations[i].lat, lng: Locations[i].lng};
+	console.log(address, Locations[i].lat, Locations[i].lng, map.zoom);
+	
+	map.setCenter({lat: Locations[i].lat, lng: Locations[i].lng});
+	map.setZoom(15);
+	console.log(map.zoom);
+	$(".locations li").css({
+		'text-decoration': 'none'
+	});
+	$(".locations li")[i].style.textDecoration = "underline";
+}
 function initMap() {
-	var uluru = {lat: -25.363, lng: 131.044};
-	var map = new google.maps.Map(document.getElementById('map'), {
-	  zoom: 4,
-	  center: uluru
-	});
-	var marker = new google.maps.Marker({
-	  position: uluru,
-	  map: map
-	});
+	GetLocations();
 }
 $(window).scroll(function() {
 	var scrolled = $(this).scrollTop();
@@ -31,7 +81,14 @@ $(window).scroll(function() {
 })
 
 $(window).ready(function(){
-	
+	$(".locations li").each(function(i) {
+		
+		$(".locations .address-" + (i + 1)).click(function(){
+			console.log("clicked clicked", i);
+			showLocation(i);
+		})
+		
+	})
 	$(window).resize(function(){
 		if($(window).innerWidth() > 768) {
 			$(".links").each(function(i) {
